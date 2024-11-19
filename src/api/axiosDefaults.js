f86497943ba34fea0a252b8da3b5bd4d8a034cfc
Sam Green <sam.green7777@yahoo.com>
2024-11-19 14:14:47 +0000

    add file to components named avatar.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://drf-api-sam-c8393f26fa4b.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
